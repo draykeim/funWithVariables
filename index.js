9944c294d5627149065f9c4e1da6092baba283c8
devon <dkeim01@gmail.com>
2022-05-01 18:44:47 -0400

    please be good enough

---
+++ index.js
@@ -0,0 +1,81 @@
+//Variables
+const a = 1;
+const b = 33;
+const c = 21;
+const d = 10;
+const e = 4;
+const f = 3;
+const g = 5;
+const h = 14;
+const i = 420;
+const j = 62;
+//Addition
+
+//#1
+const sum = a + c;
+//Result should be 22
+console.log('The sum of ' + a + ' and ' + c + ' is: ' + sum);
+
+//#2
+const sumtwo = g + f;
+//Result should be 8
+console.log('The sum of ' + g + ' and ' + f + ' is: ' + sumtwo);
+
+//Subtraction
+
+//#1
+const difference = i - j;
+//Result should be 358
+console.log('The difference between ' + i + ' and ' + j + ' is: ' + difference);
+
+//#2
+const differencetwo = d - e;
+//Result should be 6
+console.log('The difference between ' + d + ' and ' + e + ' is: ' + differencetwo);
+
+//Multiplication
+
+//#1
+const product = i * j;
+//Result should be 26040
+console.log('The product of ' + d + ' and ' + e + ' is: ' + product);
+
+//#2
+const producttwo = h * c;
+//Result should be 294
+console.log('The product of ' + h + ' and ' + c + ' is: ' + producttwo);
+
+//Division
+
+//#1
+const quotient =  b / f;
+//Result should be 11
+console.log('The quotient of ' + b + ' and ' + f + ' is: ' + quotient);
+
+//#2
+
+const quotienttwo =  i / g;
+//Result should be 84
+console.log('The quotient of ' + i + ' and ' + g + ' is: ' + quotienttwo);
+
+//Incrementals
+//#1
+var k = 10,
+l = ++k;
+console.log(l= ++k)
+
+//#2
+var z = 17,
+x = ++z;
+console.log(x= ++z)
+
+
+//Decrementals
+//#1
+var v = 90,
+n = --v;
+console.log(n = --v)
+
+var m = 47,
+p = --m;
+console.log(p = --m)
